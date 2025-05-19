186875aa76b20d3ffedf3cbd03d06dae13b3fa72
Satish Raysane <slastbench02@gmail.com>
2025-05-19 19:10:22 +0000

    working with new keyword symbol

---
+++ src/UsingSymbolKeyword.js
@@ -0,0 +1,10 @@
+const username=Symbol("username");
+let password=Symbol("Password");
+let age=Symbol("age");
+
+const user={
+    username:"Satish",
+    password:"9850",
+    age:29
+}
+console.log(user.username,user.password,user.age);
