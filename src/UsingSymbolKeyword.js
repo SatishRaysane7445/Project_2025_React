cfac0adf39ffc6eb6a1bc298aa7c3683520466fe
Satish Raysane <slastbench02@gmail.com>
2025-05-19 19:50:04 +0000

    using symbol keywords

--- src/UsingSymbolKeyword.js
+++ src/UsingSymbolKeyword.js
@@ -1,10 +1,40 @@
-const username=Symbol("username");
-let password=Symbol("Password");
-let age=Symbol("age");
+// const username=Symbol("username");
+// let password=Symbol("Password");
+// let age=Symbol("age");
 
-const user={
-    username:"Satish",
-    password:"9850",
-    age:29
+// const user={
+//     username:"Satish",
+//     password:"9850",
+//     age:29
+// }
+// console.log(user.username,user.password,user.age);
+
+const _engineId = Symbol("engineId"); // hidden, unique key
+const model=Symbol("model");
+
+const car = {
+  brand: "Toyota",
+  [model]: "Camry",
+  [_engineId]: "ENG-2384023X"
+};
+
+// Public access:
+console.log(car.brand);  // ✅ Toyota
+console.log(car[model]);  // ✅ Camry
+
+// Hidden access:
+console.log(car[_engineId]); // ✅ ENG-2384023X
+
+// Try looping over keys:
+for (let key in car) {
+  console.log(key); // ❌ _engineId will NOT show
 }
-console.log(user.username,user.password,user.age);
+
+console.log(Object.keys(car));             // ❌ Does NOT include Symbol
+console.log(Object.getOwnPropertyNames(car)); // ❌ Still no Symbol
+
+// To find it:
+console.log(Object.getOwnPropertySymbols(car)); // ✅ [ Symbol(engineId) ]
+const symbolica=Object.getOwnPropertySymbols(car);
+console.log(car[symbolica[0]]);
+console.log(car[symbolica[1]]);
